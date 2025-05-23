a4ae38965589430a27a8d1999071ffa7bcd49472
alpine1808 <manh.ld235152@sis.hust.edu.vn>
2025-05-23 16:14:55 +0700

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {DanhmucComponent} from './pages/danhmuc/danhmuc.component';
 import {HethongComponent} from './pages/hethong/hethong.component';
+import {VanbanDenComponent} from './pages/vanban/vanban-den/vanban-den.component';
+import {VanbanDiComponent} from './pages/vanban/vanban-di/vanban-di.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'danhmuc', component: DanhmucComponent},
   {path: 'hethong', component: HethongComponent},
+  {path: 'vanban/vanban-den', component: VanbanDenComponent},
+  {path: 'vanban/vanban-di', component: VanbanDiComponent},
 ];
--- src/app/pages/vanban/vanban-di/vanban-di.component.ts
+++ src/app/pages/vanban/vanban-di/vanban-di.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-vanban-di',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, CommonModule],
   templateUrl: './vanban-di.component.html',
   styleUrl: './vanban-di.component.scss'
 })
